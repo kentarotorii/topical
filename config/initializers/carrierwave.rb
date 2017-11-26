0ad1c37c2a5ab35e7e4e03b75756ea281034c18d
鳥居健太郎 <kentaro@MacBook-Air.local>
2017-11-27 00:44:02 +0900

    init unicorn

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,10 +12,10 @@ CarrierWave.configure do |config|
 
   case Rails.env
     when 'production'
-      config.fog_directory = 't-achieve-production'
+      config.fog_directory = 't-exam3-production'
       config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/t-exam3-production'
     when 'development'
-      config.fog_directory = 't-achieve-development'
+      config.fog_directory = 't-exam3-development'
       config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/t-exam3-development'
   end
 end
