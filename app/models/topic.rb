66d3190d5f6ebef9f8f284d0f6b42bd405e152ae
鳥居健太郎 <kentaro@MacBook-Air.local>
2017-11-29 21:41:33 +0900

    init unicorn

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,5 +1,5 @@
 class Topic < ActiveRecord::Base
-  validates :content, presence: true
+  validates :content, length: { in: 1..140 }
   belongs_to :user
   has_many :comments, dependent: :destroy
   mount_uploader :image, ImageUploader
